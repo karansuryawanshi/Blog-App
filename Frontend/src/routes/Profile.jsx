9c03fd9fb2fc3a306ddbb62faa7b79723a20c502
karansuryawanshi <karansuryawanshi717@gmail.com>
2024-02-22 20:05:38 +0530

    My first commit

--- Frontend/src/routes/Profile.jsx
+++ Frontend/src/routes/Profile.jsx
@@ -40,7 +40,7 @@ const Profile = () => {
                                     </div>
 
                                 ):(
-                                    <div>Username</div>
+                                    <div>username</div>
                                )
                                 }
                                 
